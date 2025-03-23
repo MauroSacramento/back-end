917e09677802ff8f94dbb089e80b75a7f146c74e
Maurosacramento <maurosacramento35@gmail.com>
2025-03-23 00:13:37 +0000

    Criar rota de autenticação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs')
 
 module.exports = {
     async store(req, res) {
@@ -11,12 +12,31 @@ module.exports = {
         }
 
         try {
+            if(await User.findOne({ email })){
+                return res.status(400).send({ msg: "User already exists!" })
+            }
+
             const user = await User.create(data);
+            user.password = undefined;
 
             return res.status(201).json({user, msg: "User created successfully"});
 
         } catch (error) {
             return res.status(500).send({error: "Registration failed"})
         }
+    },
+
+    async authentication(req, res){
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select('+password');
+
+        if(!user)
+            return res.status(400).send({ msg: "User not found!"})
+
+        if(!await bcrypt.compare(password, user.password))
+            return res.status(400).send({ msg: "Invalid password." })
+
+        return res.send({ user })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const authController = require('./controllers/authController');
 
 routes.post("/register", authController.store)
+routes.post("/authenticate", authController.authentication)
 
 module.exports = routes;
